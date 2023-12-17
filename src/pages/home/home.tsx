bcc8ceafc852819ce6ea20cf4c5352e42b7ad88a
Shuvo <safayanshuvo@gmail.com>
2023-12-18 02:25:34 +0600

    Add featured Section

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Hero } from './hero';
+import Featured from './../../components/featured/Featured';
 
 export const Home = () => {
   return (
     <>
       <Hero />
+      <Featured />
     </>
   );
 };
